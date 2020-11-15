50a20fcd5f32559733de22034d0040d66a0a6758
kartikp36 <kartikp2002@gmail.com>
2020-11-15 14:20:08 +0530

    fix: camelcase and post response

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ app.use(express.json());
 
 
 const notes = [
-    {id: 0, Title: "Demo note", Created: Date(), LastUpdated: "some time", Note: "Welcome, this is a demo note."}
+    {id: 0, title: "Demo note", created: Date(), lastUpdated: "some time", noteContent: "Welcome, this is a demo note."}
 ];
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Notes app.")
+    res.send("Welcome to notes app.")
 });
 
 // Get all notes
@@ -26,30 +26,30 @@ app.get("/notes/:id", (req, res) => {
 });
 
 // Validation
-function validateNote(note) {
+function validatenote(note) {
     const schema = Joi.object({
-      Title: Joi.string().min(1).required(),
-      Note : Joi.string().required()
+      title: Joi.string().required(),
+      noteContent : Joi.string().required()
     })
     return schema.validate(note);
   }
 
-//post: Making a new Note
+//post: Making a new note
 app.post("/notes", (req, res) => {
-  const { error } = validateNote(req.body);
+  const { error } = validatenote(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const note = {
     id: notes.length + 1,
-    Title: req.body.Title,
-    Created: Date(),
-    LastUpdated : Date(),
-    Note: req.body.Note,
+    title: req.body.title,
+    created: Date(),
+    lastUpdated : Date(),
+    noteContent: req.body.noteContent,
   };
   notes.push(note);
-  res.send(note);
+  res.status(200).send();
 });
 
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Listening at port ${port} ...`));
