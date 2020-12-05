1aad3e2b517d5f266b583bca915e686f3db6816e
kartikp36 <kartikp2002@gmail.com>
2020-12-05 14:22:29 +0530

    fix: added more tests

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,37 +1,66 @@
 const got = require("got");
+const { func } = require("joi");
 const app = require("../app");
 
 describe("http requests testing", () => {
   test("Initial GET request", async () => {
-    const response = await got(`http://localhost:5000/notes`).json();
-    expect(response).toStrictEqual([]);
+    const response = await got(`http://localhost:5000/notes`);
+    expect(response.statusCode).toStrictEqual(200);
+    noteJson = JSON.parse(response.body);
+    expect(noteJson).toStrictEqual([]);
   });
 
   test("Initial POST request", async () => {
-    const { body } = await got.post(`http://localhost:5000/notes`, {
+    const body = await got.post(`http://localhost:5000/notes`, {
       json: {
-        title: "First",
+        title: "1st",
         noteContent: "This is First note",
       },
     });
+    expect(body.statusCode).toStrictEqual(200);
   });
 
   test("GET request after POST", async () => {
-    const response = await got(`http://localhost:5000/notes`).json();
-    expect(response).toHaveLength(1);
+    const response = await got(`http://localhost:5000/notes`);
+    expect(response.statusCode).toStrictEqual(200);
+    noteJson = JSON.parse(response.body);
+    expect(noteJson.length).toStrictEqual(1);
+    expect(noteJson[0].title).toBe("1st");
+    expect(noteJson[0].noteContent).toBe("This is First note");
   });
 
   test("POST request for second note", async () => {
-    const { body } = await got.post(`http://localhost:5000/notes`, {
+    const body = await got.post(`http://localhost:5000/notes`, {
       json: {
-        title: "First",
-        noteContent: "This is First note",
+        title: "2nd",
+        noteContent: "This is Second note",
       },
     });
+    expect(body.statusCode).toStrictEqual(200);
   });
 
   test("GET request after second note is added", async () => {
-    const response = await got(`http://localhost:5000/notes`).json();
-    expect(response).toHaveLength(2);
+    const response = await got(`http://localhost:5000/notes`);
+    expect(response.statusCode).toStrictEqual(200);
+    noteJson = JSON.parse(response.body);
+    expect(noteJson.length).toStrictEqual(2);
+    expect(noteJson[1].title).toBe("2nd");
+    expect(noteJson[1].noteContent).toBe("This is Second note");
+  });
+
+  test("GET request of second note by id", async () => {
+    const response = await got(`http://localhost:5000/notes/2`);
+    expect(response.statusCode).toStrictEqual(200);
+    noteJson = JSON.parse(response.body);
+    expect(noteJson.title).toBe("2nd");
+    expect(noteJson.noteContent).toBe("This is Second note");
+  });
+
+  test("GET request by id to get 404 response", async () => {
+    const response = await got(`http://localhost:5000/notes/99`).catch(
+      (err) => {
+        expect(err.response.statusCode).toStrictEqual(404);
+      }
+    );
   });
 });
