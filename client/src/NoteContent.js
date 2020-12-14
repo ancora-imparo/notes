e14d0564595165be657e08e5e8aabff5147c2b88
Disha Patel <dishap1509@gmail.com>
2020-12-14 20:16:09 +0530

    chore: edit form

--- client/src/NoteContent.js
+++ client/src/NoteContent.js
@@ -1,47 +1,46 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-
- export const NoteContent = (props) =>  {
-  const {noteSelected} = props;
-    const useStyles = makeStyles((theme) => ({
-      root: {
-        '& .MuiTextField-root': {
-          margin: theme.spacing(1),
-          width: '25ch',
-        },
+export const NoteContent = (props) => {
+  const { noteSelected } = props;
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "25ch",
       },
-    }));
+    },
+  }));
 
   const classes = useStyles();
   const checkTitle = noteSelected ? noteSelected.title : " ";
   const [value, setValue] = React.useState(checkTitle);
   const handleChange = (event) => {
-    setValue(event.target.value);}
-  return (<div>
- 
-  <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="standard-multiline-flexible"
-          label="Title"
-          multiline
-          rowsMax={4}
-         
-          onChange={handleChange}
-        />
-        <br />
-        <TextField
-          id="standard-multiline-static"
-          label="Content"
-          multiline
-          
-          defaultValue="This is a demo note"
-        />
-      </div>
-    </form>
-  
-  </div>);
-}
-
+    setValue(event.target.value);
+  };
+  return (
+    <div>
+        <div>
+          <TextField
+            id='standard-multiline-flexible'
+            label='Title'
+            fullWidth
+            onChange={handleChange}
+          />
+          <TextField
+            id='standard-full-width'
+            style={{ margin: 8 }}
+            rowsMax={40}
+            placeholder='This is a note'
+            fullWidth
+            multiline
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+    </div>
+  );
+};
