79cf48cb1fd97bcb98e3f2d643112a59042846da
Disha Patel <dishap1509@gmail.com>
2021-02-23 16:55:13 +0530

    chore: add text editor

--- client/src/NoteContent.js
+++ client/src/NoteContent.js
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 import { Formik, Form } from "formik";
 import axios from "axios";
 import { format } from "date-fns";
+import styled from "styled-components";
+import { Editor, Floater, MenuBar } from "@aeaton/react-prosemirror";
+import { options, menu } from "@aeaton/react-prosemirror-config-default";
 
 import * as constants from "./constants";
 
@@ -24,6 +27,27 @@ const NoteContent = (props) => {
     setContent(event.target.value);
   };
 
+  const Container = styled("div")`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  `;
+  const Input = styled("div")`
+    width: 100%;
+    height: 50%;
+    overflow-y: auto;
+  `;
+  const Output = styled("pre")`
+    width: 100%;
+    height: 50%;
+    overflow-y: auto;
+    padding: 1em;
+    background: black;
+    color: lawngreen;
+  `;
+
   return (
     <Formik>
       <Form
@@ -40,7 +64,8 @@ const NoteContent = (props) => {
           } catch (err) {
             console.log(err.response);
           }
-        }}>
+        }}
+      >
         <TextField
           id="title"
           label="Title"
@@ -48,6 +73,33 @@ const NoteContent = (props) => {
           fullWidth
           onChange={handleTitleChange}
         />
+        <Container>
+          <Input>
+            <Editor
+              options={options}
+              onChange={(doc) => {
+                document.getElementById("output").textContent = JSON.stringify(
+                  doc,
+                  null,
+                  2
+                );
+              }}
+              render={({ editor, view }) => (
+                <React.Fragment>
+                  <MenuBar menu={menu} view={view} />
+
+                  <Floater view={view}>
+                    <MenuBar menu={{ marks: menu.marks }} view={view} />
+                  </Floater>
+
+                  {editor}
+                </React.Fragment>
+              )}
+            />
+          </Input>
+          <Output id={"output"} />
+        </Container>
+        ,
         <TextField
           id="content"
           label="Content"
@@ -65,7 +117,8 @@ const NoteContent = (props) => {
             position: "absolute",
             bottom: "0px",
             width: "100%",
-          }}>
+          }}
+        >
           <button>Cancel</button>
           {format(new Date(noteSelected.created), ` ${constants.TIME_FORMAT}`)}
           <button style={{ float: "right" }} type="submit">
