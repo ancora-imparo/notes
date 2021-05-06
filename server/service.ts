4e33ff1e5a5ffdffd637809aa6cbd8222958188c
kartikp36 <kartikp2002@gmail.com>
2021-05-06 11:44:47 +0530

    refactor: move store.ts to service.ts and initialise pg only once at server.ts

--- server/service.ts
+++ server/service.ts
@@ -1,27 +1,63 @@
 import _ from "lodash";
+import { pool } from "./server";
 
-import * as store from "./store";
+const readNotes = async (): Promise<Note[]> => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM notes");
+    const { results } = { results: result ? result.rows : null };
+    client.release();
+    return results!;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
 
+const saveNoteQuery = async (note: Note, sqlQuery): Promise<void> => {
+  try {
+    const client = await pool.connect();
+    await client.query(sqlQuery);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+export const initialiseSQLTable = async (): Promise<void> => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `CREATE TABLE IF NOT EXISTS notes(id serial primary key,title varchar(30) not null , "noteContent" text not null,created text,  "lastUpdated"  text)`
+    );
+    client.release();
+  } catch (err) {
+    console.error(err);
+  }
+};
 export const getAllNotes = async (): Promise<Note[]> => {
-  const notes = await store.readNotes();
+  const notes = await readNotes();
   return notes;
 };
 
 export const getNoteById = async (id: number): Promise<Note | undefined> => {
-  const notes = await store.readNotes();
+  const notes = await readNotes();
   const note = notes.find((element) => element.id === id);
   return note;
 };
 
 export const saveNote = async (note: Note): Promise<number> => {
-  const notes = await store.readNotes();
+  const notes = await readNotes();
   const noteIndex = _.findIndex(notes, (n: Note) => n.id === note.id);
   if (noteIndex >= 0) {
     const updatedNote = { ...notes[noteIndex], ...note };
     updatedNote.lastUpdated = Date();
-    store.updateNote(updatedNote);
+    saveNoteQuery(
+      updatedNote,
+      `UPDATE notes SET title = '${updatedNote.title}', "noteContent" = '${updatedNote.noteContent}' , "lastUpdated" = '${updatedNote.lastUpdated}' WHERE id = ${updatedNote.id};`
+    );
   } else {
-    note.id = Math.max(0, ...notes.map((item) => item.id)) + 1;
     const newNote = {
       id: note.id,
       title: note.title,
@@ -29,7 +65,10 @@ export const saveNote = async (note: Note): Promise<number> => {
       lastUpdated: Date(),
       noteContent: note.noteContent,
     };
-    store.insertNote(newNote);
+    saveNoteQuery(
+      newNote,
+      `INSERT INTO notes(title, "noteContent", created, "lastUpdated") values('${newNote.title}', '${newNote.noteContent}', '${newNote.created}','${newNote.lastUpdated}')`
+    );
   }
   return note.id;
 };
