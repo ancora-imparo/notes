7e10713ca8b3921b55917d6f883bb829788e4b13
kartikp36 <kartikp2002@gmail.com>
2021-05-07 17:27:31 +0530

    fix: TIMESTAMPZ in pg, formatting and some other improvements

--- server/service.ts
+++ server/service.ts
@@ -6,19 +6,33 @@ const handleSQLQuery = async (sqlQuery) => {
   try {
     const client = await pool.connect();
     const result = await client.query(sqlQuery);
-    const { results } = { results: result ? result.rows : null };
+    const results = _.get(result, "rows", null);
     client.release();
-    return results!;
+    return results;
   } catch (err) {
-    console.error(err);
-    return err;
+    throw new Error(
+      _.get(err, "message", "Error occurred at handleSQLQuery()")
+    );
+  }
+};
+const handleSaveNote = async (sqlQuery, values) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(sqlQuery, values);
+    const results = _.get(result, "rows", null);
+    client.release();
+    return results;
+  } catch (err) {
+    throw new Error(
+      _.get(err, "message", "Error occurred at handleSQLQuery()")
+    );
   }
 };
 
 export const initialiseSQLTable = async (): Promise<void> => {
   await handleSQLQuery(
-    `CREATE TABLE IF NOT EXISTS notes(id serial primary key,title varchar(30) not null , "noteContent" text not null,created text,  "lastUpdated"  text);
-    CREATE INDEX IF NOT EXISTS index ON notes(id)`
+    `CREATE TABLE IF NOT EXISTS notes(id SERIAL PRIMARY KEY, title VARCHAR(30) NOT NULL, "noteContent" TEXT NOT NULL, created TIMESTAMPTZ, "lastUpdated" TIMESTAMPTZ);
+    CREATE UNIQUE INDEX IF NOT EXISTS index ON notes(id);`
   );
 };
 
@@ -28,19 +42,25 @@ export const getAllNotes = async (): Promise<Note[]> => {
 };
 
 export const getNoteById = async (id: number): Promise<Note | undefined> => {
-  const notes = await handleSQLQuery(`SELECT * FROM notes`);
-  const note = notes.find((element) => element.id === id);
+  const [note] = await handleSQLQuery(`SELECT * FROM notes WHERE id = ${id}`);
   return note;
 };
 
 export const saveNote = async (note: Note): Promise<number> => {
-  const notes: Note[] = await handleSQLQuery(`SELECT * FROM notes`);
-  const noteIndex = _.findIndex(notes, (n) => n.id === note.id);
-  if (noteIndex >= 0) {
-    const updatedNote = { ...notes[noteIndex], ...note };
+  const [noteExists] = await handleSQLQuery(
+    `SELECT * FROM notes WHERE id = ${note.id}`
+  );
+  if (noteExists) {
+    const updatedNote = { ...noteExists, ...note };
     updatedNote.lastUpdated = Date();
-    handleSQLQuery(
-      `UPDATE notes SET title = '${updatedNote.title}', "noteContent" = '${updatedNote.noteContent}' , "lastUpdated" = '${updatedNote.lastUpdated}' WHERE id = ${updatedNote.id};`
+    await handleSaveNote(
+      `UPDATE notes SET title = $1, "noteContent" = $2 , "lastUpdated" = $3 WHERE id = $4;`,
+      [
+        updatedNote.title,
+        updatedNote.noteContent,
+        new Date(updatedNote.lastUpdated),
+        updatedNote.id,
+      ]
     );
   } else {
     const newNote = {
@@ -50,8 +70,14 @@ export const saveNote = async (note: Note): Promise<number> => {
       lastUpdated: Date(),
       noteContent: note.noteContent,
     };
-    handleSQLQuery(
-      `INSERT INTO notes(title, "noteContent", created, "lastUpdated") values('${newNote.title}', '${newNote.noteContent}', '${newNote.created}','${newNote.lastUpdated}')`
+    await handleSaveNote(
+      `INSERT INTO notes(title, "noteContent", created, "lastUpdated") VALUES($1, $2, $3, $4)`,
+      [
+        newNote.title,
+        newNote.noteContent,
+        new Date(newNote.created),
+        new Date(newNote.lastUpdated),
+      ]
     );
   }
   return note.id;
@@ -59,7 +85,7 @@ export const saveNote = async (note: Note): Promise<number> => {
 
 export const deleteNoteById = async (id: number): Promise<boolean> => {
   const [deleted] = await handleSQLQuery(
-    `DELETE FROM notes where id=${id} RETURNING id`
+    `DELETE FROM notes WHERE id=${id} RETURNING id`
   );
   return !!deleted;
 };
